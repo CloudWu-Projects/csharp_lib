eb15023dbb37485f47d7f55bcda57d8b050fbbb2
Dai Li <d.li@simplylive.tv>
2023-03-24 10:50:37 +0800

    add crop

---
+++ baseLib/PictrueCrop.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lib.baseLib
+{
+    internal class PictrueCrop
+    {
+
+       
+        /// <summary>
+        /// 裁剪图片
+        /// </summary>
+        /// <param name="originImage">原图片</param>
+        /// <param name="region">裁剪的方形区域</param>
+        /// <returns>裁剪后图片</returns>
+        public static Image CropImage(Image originImage, Rectangle region)
+        {
+            Bitmap result = new Bitmap(region.Width, region.Height);
+            Graphics graphics = Graphics.FromImage(result);
+            graphics.DrawImage(originImage, new Rectangle(0, 0, region.Width, region.Height), region, GraphicsUnit.Pixel);
+            return result;
+        }
+    }
+}
