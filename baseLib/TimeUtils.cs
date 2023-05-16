d062098767b2e5e94db92bf875fcebad1c845256
Dai Li <d.li@simplylive.tv>
2023-05-16 11:38:30 +0800

    add timeutils

---
+++ baseLib/TimeUtils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wu_jiaxing20220115.csharp_lib.baseLib
+{
+    internal class TimeUtils
+    {
+       static public DateTime ConvertLongToDateTime(long unixTimeStamp)
+        {
+            System.DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1)); // 当地时区
+            DateTime dt = startTime.AddSeconds(unixTimeStamp);
+            return dt;
+        }
+    }
+}
