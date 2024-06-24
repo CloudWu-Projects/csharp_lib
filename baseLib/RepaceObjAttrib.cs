fb45f5942f8fa179340395e8933c6e0426d00a24
Dai Li <d.li@simplylive.tv>
2024-06-24 16:53:04 +0800

    update

--- baseLib/RepaceObjAttrib.cs
+++ baseLib/RepaceObjAttrib.cs
@@ -6,13 +6,15 @@ namespace csharp_lib.baseLib
         internal static string replaceObjAtrribe(object datum, ref string sql)
         {
             var properties = datum.GetType().GetProperties();
+            var newSql = sql.ToLower();
             foreach (var property in properties)
             {
                 var name = property.Name;
                 var value = property.GetValue(datum);
                 if (value != null)
                 {
-                    sql = sql.Replace($"{{json.{name}}}".ToLower(), value.ToString());
+                    sql = newSql.Replace($"{{json.{name}}}".ToLower(), value.ToString())
+                        .Replace($"json.{name}".ToLower(), value.ToString());
                 }
             }
             return sql;
