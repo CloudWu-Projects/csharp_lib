b0303a32db2cc48daa8667f8618ef43eb92072e4
Dai Li <li.dai@riedel.net>
2024-07-24 15:05:10 +0800

    add template

--- baseLib/ColumnMappingAttribute.cs
+++ baseLib/ColumnMappingAttribute.cs
@@ -6,6 +6,12 @@ using System.Threading.Tasks;
 
 namespace wu_jiaxing20220115
 {
+    class Template
+    {       
+        [ColumnMapping("incodenumber")]
+        public string gateNo_in {get;set; }    
+    }
+
     [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
     public sealed class ColumnMappingAttribute:Attribute
     {
