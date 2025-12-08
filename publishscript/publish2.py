1ba5d1d7c50a0d68bc479fb468b530b13a7c2c43
Dai li <li.dai@riedel.net>
2025-12-08 19:38:20 +0800

    fix(publishscript): add UTF-8 encoding for stdout and stderr

--- publishscript/publish2.py
+++ publishscript/publish2.py
@@ -1,4 +1,7 @@
 
+import sys,io
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8', errors='replace')
+sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8', errors='replace')
 
 from sendMailBase import sendFolder
 
